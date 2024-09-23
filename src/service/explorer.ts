c8e720f2349b158fc8feb6b5d27b87c19929f0dd
auuunya <zyy.im@outlook.com>
2024-09-23 14:30:00 +0800

    build: deploy vercel

--- src/service/explorer.ts
+++ src/service/explorer.ts
@@ -1,11 +1,6 @@
-// 引入 xml 库（如有需要，可通过 npm 安装 xml 库）
-import { writeFileSync } from 'fs';
 import { UserFeed } from "../types/view"
-// OPML 生成函数
 export function generateOpml(data: UserFeed[]): string {
   const now = new Date().toISOString();
-
-  // 开始构建 OPML 字符串
   let opml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
   opml += `<opml version="2.0">\n`;
   opml += `  <head>\n`;
@@ -14,7 +9,6 @@ export function generateOpml(data: UserFeed[]): string {
   opml += `  </head>\n`;
   opml += `  <body>\n`;
   
-  // 遍历数据并生成 outline
   data.forEach((item) => {
     const feed = item.feeds;
     const category = item.category || "Uncategorized";
